549985996d2e81db9f3dc918d7ce8d0c4aa91374
luchosetton <lsetton@udesa.edu.ar>
2023-04-04 13:17:58 -0300

    prod controller

--- pro-Integ/controllers/productsControllers.js
+++ pro-Integ/controllers/productsControllers.js
@@ -1,12 +1,14 @@
+const data = require("../data/data");
+
 const productController = {
     product: function(req, res) {
-        return res.render('product');
+        return res.render('product',{producto: data.productos[0],comentarios: data.comentarios});
       },
     agregarProducto:  function(req, res) {
-        return res.render('product-add');
+        return res.render('product-add',{datosUsuario: data.usuario});
       },
     detalle:  function(req, res) {
-        return res.render('search-results');
+        return res.render('search-results',{producto: data.productos[0],comentarios: data.comentarios});
       }
 }
 module.exports = productController
