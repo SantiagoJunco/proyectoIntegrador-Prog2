fae40e4030475bb491e239d10d8bdf733ac93802
nazafloresm <nfloresmunoz@udesa.edu.ar>
2023-04-04 13:36:16 -0300

    index rutas

--- pro-Integ/routes/index.js
+++ pro-Integ/routes/index.js
@@ -5,10 +5,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', indexController.index);
 
-router.get('/headerlogueado', indexController.headerLogueado);
-
 router.get('/login', indexController.login);
 
 router.get('/register',indexController.register);
 
 module.exports = router;
+
