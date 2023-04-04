a9a6b40b5321a99cef86a27a50f4f220aaf24cbe
luchosetton <lsetton@udesa.edu.ar>
2023-04-04 13:13:12 -0300

    data

---
+++ pro-Integ/data/data.js
@@ -0,0 +1,103 @@
+const data = {
+    usuario: {
+        email: "juan.perez@gmail.com",
+        nombreUsuario: "juanperez",
+        contraseña: "juan99",
+        fechaNacimiento: "15/02/1999",
+        dni: "38756390",
+        fotoPerfil: "/images/users/default-image.png"
+      },
+
+    productos: [
+        {
+            imagen: "/images/products/iphone14ProMax.jpg",
+            nombreProducto: "iPhone 14 Pro Max",
+            descripcion: "Pantalla de 6.7 pulgadas, procesador A16 Bionic, triple cámara trasera y un diseño renovado.",
+            fechaCarga: "15 de noviembre de 2022"
+        },
+        {
+            imagen: "/images/products/iphone-14-pro.jpg",
+            nombreProducto: "iPhone 14 Pro",
+            descripcion: "Pantalla de 6.1 pulgadas, procesador A16 Bionic, triple cámara trasera y características de gama alta.",
+            fechaCarga: "1 de octubre de 2022"
+        },
+        {
+            imagen: "/images/products/iphone14.jpg",
+            nombreProducto: "iPhone 14",
+            descripcion: "Pantalla de 6.1 pulgadas, procesador A16 Bionic, cámara dual trasera y un diseño actualizado.",
+            fechaCarga: "22 de septiembre de 2022"
+        },
+        {
+            imagen: "/images/products/iphone13ProMax.jpg",
+            nombreProducto: "iPhone 13 Pro Max",
+            descripcion: "Pantalla de 6.7 pulgadas, procesador A15 Bionic, triple cámara trasera y funciones avanzadas de video.",
+            fechaCarga: "7 de diciembre de 2021"
+        },
+        {
+            imagen: "/images/products/iphone13Pro.jpg",
+            nombreProducto: "iPhone 13 Pro",
+            descripcion: "Pantalla de 6.1 pulgadas, procesador A15 Bionic, triple cámara trasera y características avanzadas de fotografía.",
+            fechaCarga: "30 de septiembre de 2021"
+        },
+        {
+            imagen: "/images/products/iphone13.jpg",
+            nombreProducto: "iPhone 13",
+            descripcion: "Pantalla de 6.1 pulgadas, procesador A15 Bionic, cámara dual trasera y una batería mejorada.",
+            fechaCarga: "10 de noviembre de 2021"
+        },
+        {
+            imagen: "/images/products/iphoneSE(2daG).jpg",
+            nombreProducto: "iPhone SE (2G)",
+            descripcion: "Segunda generación del iphone SE. Pantalla de 4.7 pulgadas, procesador A13 Bionic, cámara trasera de 12 megapíxeles y un precio más asequible.",
+            fechaCarga: "8 de junio de 2020"
+        },
+        {
+            imagen: "/images/products/iphone11ProMax.jpg",
+            nombreProducto: "iPhone 11 Pro Max",
+            descripcion: "Pantalla de 6.5 pulgadas, procesador A13 Bionic, triple cámara trasera y un diseño renovado.",
+            fechaCarga: "20 de octubre de 2019"
+        },
+        {
+            imagen: "/images/products/iphone11Pro.jpg",
+            nombreProducto: "iPhone 11 Pro",
+            descripcion: "Pantalla de 5.8 pulgadas, procesador A13 Bionic, triple cámara trasera y mejoras en la duración de la batería.",
+            fechaCarga: "12 de noviembre de 2019"
+        },
+        {
+            imagen: "/images/products/iphone11.jpg",
+            nombreProducto: "iPhone 11",
+            descripcion: "Pantalla de 6.1 pulgadas, procesador A13 Bionic, cámara dual trasera y mejoras en la calidad de la fotografía",
+            fechaCarga: "6 de febrero de 2020"
+        }
+    ],
+
+    comentarios: [
+        {
+          nombreUsuario: "maria_gomez",
+          textoComentario: "Acabo de comprar el nuevo iPhone y estoy muy contenta con su rendimiento, sin duda es una excelente opción.",
+          imagenPerfil: "/images/users/default-image.png"
+        },
+        {
+          nombreUsuario: "luis_perez",
+          textoComentario: "La calidad de la cámara del iPhone es impresionante.",
+          imagenPerfil: "/images/users/default-image.png"
+        },
+        {
+          nombreUsuario: "juan_martinez",
+          textoComentario: "Me encanta el modelo! super práctico, buen tamaño para llevar en el bolsillo.",
+          imagenPerfil: "/images/users/default-image.png"
+        },
+        {
+          nombreUsuario: "ana_rodriguez",
+          textoComentario: "El precio del iPhone puede ser un poco elevado, pero su calidad y rendimiento lo hacen valer la pena.",
+          imagenPerfil: "/images/users/default-image.png"
+        },
+        {
+          nombreUsuario: "carlos_gonzalez",
+          textoComentario: "La verdad que prefiero el rendimiento de los Samsung, son mas faciles de manejar.",
+          imagenPerfil: "/images/users/default-image.png"
+        }
+      ]
+      
+}
+module.exports = data
