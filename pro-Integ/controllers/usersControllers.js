6ab32201589055673fdecb8a94bae9d02ccbceb4
luchosetton <lsetton@udesa.edu.ar>
2023-06-01 11:47:46 -0300

    user controler

--- pro-Integ/controllers/usersControllers.js
+++ pro-Integ/controllers/usersControllers.js
@@ -1,11 +1,37 @@
 const data = require("../data/data");
+const db = require("../database/models");
+const Usuario = db.Usuario
 
 const userController = {
-    profile:function(req, res) {
-        return res.render('profile',{datosUsuario: data.usuario, productos: data.productos, comentarios: data.comentarios });
-      },
-    profileEdit: function(req, res) {
-        return res.render('profile-edit',{datosUsuario: data.usuario});
-      }
+  profile: function (req, res) {
+    let rel = { include: [
+      { association: "usuarioProducto"}
+  ]
+  }
+    let id = req.params.id
+    Usuario.findByPk(id, rel, { raw: true, nest: true })
+      .then((data) => {
+        console.log(data);
+        return res.render('profile', {data}) 
+      })
+      .catch((error) => {
+        return console.log(error);
+      })
+  },
+  profileEdit: function (req, res) {
+    let rel = { include: [
+      { association: "usuarioProducto"}
+  ]
+  }
+    let id = req.params.id
+    Usuario.findByPk(id, rel, { raw: true, nest: true })
+      .then((data) => {
+        console.log(data);
+        return res.render('profile-edit', {data}) 
+      })
+      .catch((error) => {
+        return console.log(error);
+      })
+  }
 }
 module.exports = userController
