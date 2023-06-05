40dad6c0032756862aa1d0057a3de2f79399e7ed
SantiagoJunco <Jsantiagoemilio@udesa.edu.ar>
2023-06-05 10:07:23 -0300

    validaciones formularios

--- pro-Integ/controllers/indexControllers.js
+++ pro-Integ/controllers/indexControllers.js
@@ -1,6 +1,7 @@
 const data = require("../data/data");
 const db = require("../database/models")
 const Producto = db.Producto
+const Usuario = db.Usuario
 
 const indexController = {
     index: function(req, res) {
@@ -21,7 +22,42 @@ const indexController = {
         return res.render('login');
       },
     register:  function(req, res) {
-        return res.render('register');
+        return res.render('register',{error: null});
+      },
+    registroDatos: function(req, res) {
+      let emailForm = req.body.email
+      let passForm = req.body.contraseña
+
+      if (emailForm == "") {
+        return res.render('register', {error: 'Debe introducir un email'})
+
+      }else if (passForm.length < 4) {
+        return res.render('register', {error: 'La contraseña debe tener más de 3 caracteres'})
+      } 
+      else {
+        Usuario.findOne({where: [
+          {email: emailForm}
+          ]
+          })
+            .then((data) =>{
+              console.log(data);
+              if (data != undefined) {
+                if (data.email == emailForm) {
+                  return res.render('register', {error: 'El email ya está registrado'})
+                }
+              }// crear usuario 
+              
+        
+  
+              
+  
+               
+            })
+            .catch((error)=>{
+              return console.log(error);
+            })
       }
+     
+    }
 }
 module.exports = indexController
--- pro-Integ/routes/index.js
+++ pro-Integ/routes/index.js
@@ -6,8 +6,11 @@ var router = express.Router();
 router.get('/', indexController.index);
 
 router.get('/login', indexController.login);
+router.post('/login/create',indexController.registroDatos)
 
 router.get('/register',indexController.register);
+
+
 
 module.exports = router;
 
